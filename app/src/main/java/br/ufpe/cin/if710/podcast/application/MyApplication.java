382b2847e1a8c7c0a36f353a2267de6e4587d0b2
rbs7 <rbs7@cin.ufpe.br>
2017-12-13 15:35:58 -0300

    Instalado AndroidDevMetrics

--- app/src/main/java/br/ufpe/cin/if710/podcast/application/MyApplication.java
+++ app/src/main/java/br/ufpe/cin/if710/podcast/application/MyApplication.java
@@ -2,9 +2,22 @@ package br.ufpe.cin.if710.podcast.application;
 
 import android.app.Application;
 
+import com.frogermcs.androiddevmetrics.AndroidDevMetrics;
+
+import br.ufpe.cin.if710.podcast.BuildConfig;
+
 /**
  * Created by Rogerio Santos on 13/12/2017.
  */
 
 public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //Use it only in debug builds
+        if (BuildConfig.DEBUG) {
+            AndroidDevMetrics.initWith(this);
+        }
+    }
 }
+
