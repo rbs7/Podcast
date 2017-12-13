b9946a3ee9f60f8f9fcbaecc8f9eea52781d768a
rbs7 <rbs7@cin.ufpe.br>
2017-12-13 13:46:25 -0300

    Definida classe Application

---
+++ app/src/main/java/br/ufpe/cin/if710/podcast/application/MyApplication.java
@@ -0,0 +1,10 @@
+package br.ufpe.cin.if710.podcast.application;
+
+import android.app.Application;
+
+/**
+ * Created by Rogerio Santos on 13/12/2017.
+ */
+
+public class MyApplication extends Application {
+}
